1b287e1e9ca072dc73f6da4c2b208ce2ce64d293
hyunalee <dlgusdk419@naver.com>
2019-10-24 11:57:53 +0900

    feat(user-inputs): add examples

--- user-inputs/App.js
+++ user-inputs/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+const Input = props => (
+  <View style={styles.textInputContainer}>
+    <Text style={styles.textInputLabel}>
+      {props.label}
+    </Text>
+    <TextInput
+      style={styles.textInput}
+      {...props}
+    />
+  </View>
+);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Input label="Basic Text Input:" />
+
+      <Input label="Password Input:" secureTextEntry />
+
+      <Input label="Return Key:" returnKeyType="search" />
+
+      <Input label="Placeholder Text:" placeholder="Search" />
+
+      <Input
+        label="Input Events:"
+        onChangeText={(e) => {console.log(e)}}
+        onSubmitEditing={(e) => {console.log('Submit: ', e)}}
+        onFocus={() => console.log('focus')}
+      />
     </View>
   );
 }
@@ -12,8 +37,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#efefef',
     justifyContent: 'center',
   },
+  textInputContainer: {
+    margin: 10
+  },
+  textInputLabel: {
+    fontWeight: 'bold'
+  },
+  textInput: {
+    backgroundColor: '#fff'
+  }
 });
