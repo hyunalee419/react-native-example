1a40fdc954af88a6f6459827daf2a79c831add27
hyunalee <dlgusdk419@naver.com>
2019-11-04 13:56:53 +0900

    feat: add ResizingImages

--- print-image/ResizingImages.js
+++ print-image/ResizingImages.js
@@ -2,48 +2,63 @@ import React from 'react';
 import { StyleSheet, View, Text, Image, Slider } from 'react-native';
 
 const ResizingImages = ({
-  reactSource,
-  relaySource,
+  source
 }) => {
 	const [ width, setWidth ] = React.useState(100);
 	const [ height, setHeight ] = React.useState(100);
 	return (
 		<View style={styles.container}>
-			<Image
-				style={{ width, height }}
-				source={relaySource}
-			/>
-
-			<Text>Width: {width}</Text>
-			<Text>Height: {height}</Text>
-
-			<Slider
-				style={styles.slider}
-				minimumValue={30}
-				maximumValue={150}
-				value={width}
-				onValueChange={(v) => {
-					setWidth(v);
-					setHeight(v);
-				}}
-			/>
+			<View style={styles.imageContainer}>
+				<Image
+					source={source}
+					style={{ width, height }}
+				/>
+			</View>
+			<View style={styles.sliderContainer}>
+				<Text>Width: {width}</Text>
+				<Text>Height: {height}</Text>
+				<Slider
+					style={styles.slider}
+					minimumValue={50}
+					maximumValue={150}
+					value={width}
+					onValueChange={(v) => {
+						setWidth(v);
+						setHeight(v);
+					}}
+				/>
+			</View>
 		</View>
 	)
 };
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	imageContainer: {
+		flex: 1,
+		width: '100%',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	sliderContainer: {
+		flex: 1,
+		width: '100%',
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'ghostwhite',
 	},
 	slider: {
-
+		width: 100
 	}
 })
 
 ResizingImages.defaultProps = {
-	reactSource: {
+	source: {
 		uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png',
 	},
-	relaySource: require('./images/coinbox.png'),
 };
 export default ResizingImages;
