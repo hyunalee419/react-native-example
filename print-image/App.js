3c4dd8361445abada48b624b49b81c5bf8a3839d
hyunalee <dlgusdk419@naver.com>
2019-11-07 15:22:51 +0900

    feat: add RenderingIcons

--- print-image/App.js
+++ print-image/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import LoadingImages from './examples/LoadingImages';
-export default () => <LoadingImages/>;
+// import LoadingImages from './examples/LoadingImages';
+// export default () => <LoadingImages/>;
 
 // import ResizingImages from './examples/ResizingImages';
 // export default () => <ResizingImages/>;
 
 // import LazyLoading from './examples/LazyLoading';
 // export default () => <LazyLoading/>
+
+import RenderingIcons from './examples/RenderingIcons';
+export default () => <RenderingIcons/>;
